97ce4366725b74e55e3cab9913f7edd05644627c
srikanth-52 <nampellisrikanth52@gmail.com>
2026-09-13 12:08:22 +0530

    Configure project for Vercel deployment

--- Fraud/settings.py
+++ Fraud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd^(g$2aybskv($5l!ai46yo_@@d1x7tc2px&h_%z$nr*bho$dw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'FraudApp', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
